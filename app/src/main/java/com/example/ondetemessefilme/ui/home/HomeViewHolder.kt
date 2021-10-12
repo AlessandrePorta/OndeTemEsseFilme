5878df9f486bc00abe2fc2e3a1d0e141141f48a3
Alessandre <ale_junior97@hotmail.com>
2021-10-12 00:54:44 -0300

    HomeScreen fixed, colors changed

--- app/src/main/java/com/example/ondetemessefilme/ui/home/HomeViewHolder.kt
+++ app/src/main/java/com/example/ondetemessefilme/ui/home/HomeViewHolder.kt
@@ -7,7 +7,7 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.movies_home_list.view.*
 
 class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    val title = itemView.movie_vote_average
+    val title = itemView.movie_title
     val voteAverage = itemView.movie_vote_average
     val movieImg = itemView.movie_img
 
@@ -15,8 +15,7 @@ class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         title.text = movies.title
         voteAverage.text = movies.vote_average.toString()
         Glide.with(itemView.context)
-            .load(movies.poster_path)
-            .fitCenter()
+            .load("https://image.tmdb.org/t/p/original" + movies.poster_path)
             .into(movieImg)
     }
 }
